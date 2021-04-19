a9b17ec34644072dd52d6c4f41b7bd98a1fa8c69
Wuu-Server <1249732457@qq.com>
2021-04-19 22:52:11 +0800

    修改了网络接口

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export function request(config, method) {
   const instance = axios.create({
-    baseURL: "http://172.17.169.27:8080",
+    baseURL: "http://attendance.keepdev.top/api",
     timeout: 5000,
     method: method,
   })
