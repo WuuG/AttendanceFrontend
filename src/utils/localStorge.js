fdcf46e920946cdcbe602129ae094bb66e384799
Wuu-Server <1249732457@qq.com>
2021-06-27 23:12:32 +0800

    📝 添加localStorge.js 用于本地存储

---
+++ src/utils/localStorge.js
@@ -0,0 +1,18 @@
+const setLocalStorge = (key, value) => {
+  const valueStr = JSON.stringify(value)
+  window.localStorage.setItem(key, valueStr)
+}
+const getLocalStorge = (key) => {
+  const str = window.localStorage.getItem(key)
+  const value = JSON.parse(str)
+  return value
+}
+const KEY = {
+  ROLE: 'role'
+}
+
+export {
+  setLocalStorge,
+  getLocalStorge,
+  KEY
+}
