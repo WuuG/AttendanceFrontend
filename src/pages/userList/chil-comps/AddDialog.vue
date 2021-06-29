903ef35989e95633576246b5327d25fc9d02135b
Wuu-Server <1249732457@qq.com>
2021-06-29 13:33:00 +0800

    🚧 添加用户

---
+++ src/pages/userList/chil-comps/AddDialog.vue
@@ -0,0 +1,87 @@
+<template>
+  <el-dialog :title="title" width="400px" :visible.sync="visible" :before-close="cancel" @open="open">
+    <el-form :model="form" :rules="rules" ref="form">
+      <el-form-item label="真实姓名" :label-width="labelWidth">
+        <el-col :span="20">
+          <el-input v-model="form.realName"> </el-input>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="性别" :label-width="labelWidth" prop="value">
+        <sex-form-item v-model="form.genderValue"></sex-form-item>
+      </el-form-item>
+      <el-form-item label="学校学院" :label-width="labelWidth">
+        <el-col :span="20">
+          <org-form-item v-model="form.schoolMajorID" :name="form.schoolMajorName"></org-form-item>
+        </el-col>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button @click="cancel">取 消</el-button>
+      <el-button type="primary" @click="submitForm('form')" :loading="buttonLoading">确 定</el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+import SexFormItem from '../../../components/context/form-item/Sex.vue';
+import OrgFormItem from '../../../components/context/form-item/Organization.vue';
+
+export default {
+  data() {
+    // value的表单验证。
+    // const validateValue = (rule, value, callback) => {
+    // };
+    return {
+      labelWidth: '90px',
+      title: '添加用户',
+      form: {},
+      rules: {
+        // value: [{ validator: validateValue, trigger: 'blur' }]
+      }
+    };
+  },
+  components: {
+    SexFormItem,
+    OrgFormItem
+  },
+  props: {
+    // 通过.sync 处理
+    visible: Boolean,
+    // 获取活跃的明细，来防止判重时，和自己重而报错。
+    active: Object
+  },
+  methods: {
+    // 网络方法
+    // 页面逻辑
+    // 组件通信
+    // 发送关闭dialog事件，按钮处理和reset表单
+    cancel(done) {
+      this.$emit('update:visible', false);
+      this.buttonLoading = false;
+      this.resetForm();
+    },
+    // 表单验证，并发送submit事件参数为表单内容，并调用cancel()
+    submitForm(refName) {
+      this.$refs[refName].validate((valid) => {
+        if (!valid) return;
+        this.buttonLoading = true;
+        this.$emit('submit', { ...this.form });
+        this.cancel();
+      });
+    },
+    // 重置表单
+    resetForm() {
+      for (const propName in this.form) {
+        this.form[propName] = null;
+      }
+    },
+    open() {
+      this.form = { ...this.active };
+      this.buttonLoading = false;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
