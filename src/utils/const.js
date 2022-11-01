13a14dc219fd9a8a586e4cfe15fcfc9878f7aaf0
wuu-lab-pc <1249732457@qq.com>
2022-11-01 14:22:27 +0800

    :bug: 修改登录时的正则判断,现在可以验证邮箱

--- src/utils/const.js
+++ src/utils/const.js
@@ -10,16 +10,18 @@ export default {
     PHONE_NUM: /^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$/,
     PASSWORD: /^[a-zA-Z0-9~!@#$%^&*()+{}|:"<>?,./;'_]{6,20}$/,
     SMSCODE: /^\d{4,6}$/,
-    ACCOUNT: /(^[a-zA-Z]([a-zA-Z0-9_]{3,19})$)|(^(\+86)?1[3-9]\d{9}$)|(^[a-zA-Z][a-zA-Z0-9]{2,19}@([a-zA-Z0-9]\.)+[a-zA-Z0-9]{2,}$)/
+    ACCOUNT: /(^[a-zA-Z]([a-zA-Z0-9_]{3,19})$)|(^(\+86)?1[3-9]\d{9}$)|(^[a-zA-Z0-9][a-zA-Z0-9]{2,19}@([a-zA-Z0-9]{1,}\.)+[a-zA-Z0-9]{2,}$)/
   },
   RE_TEXT: {
     PHONE_NUM: '请输入正确的手机号!',
     PASSWORD: '仅支持字母，数字与常见特殊符号',
     SMSCODE: '请输入4-6位验证码',
-    ACCOUNT: '请输入正确的用户名、邮箱、手机号等',
+    ACCOUNT: '请输入正确的用户名、手机号等',
   },
-  IMG_BASEURL: 'http://attendance.keepdev.top/api',
-  // IMG_BASEURL: 'http://172.17.169.27:8080',
-  BASEURL: 'http://ad.keepdev.top:8080',
-  // BASEURL :'http://attendance.keepdev.top/api'
+  // IMG_BASEURL: 'http://attendance.keepdev.top/api',
+  IMG_BASEURL: 'http://120.78.228.248:8080',
+
+  // BASEURL: 'http://ad.keepdev.top:8080',
+  // BASEURL: 'http://attendance.keepdev.top/api'
+  BASEURL: 'http://120.78.228.248:8080',
 }
